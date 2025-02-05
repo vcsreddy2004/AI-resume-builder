dece35dbda2cf3f993f38b6967893f35353a68c9
techchandu <vcsreddy2004@gmail.com>
2025-02-05 18:34:34 +0530

    user router setup complete

--- router/config.ts
+++ router/config.ts
@@ -1,5 +1,5 @@
 let HOST_NAME:string = "127.0.0.1";
 let PORT:number = 5500;
 let MONGO_DB_URL:string = "mongodb://localhost:27017/react-resume-builder";
-let STUDENT_SECRETE_KEY:string = "40p4r0p5t0plws0pqs0prf0tg0pedf0srgv4wrfb0eof4wgs0pq0pq0";
-export default {HOST_NAME,PORT,MONGO_DB_URL,STUDENT_SECRETE_KEY}
+let SECRETE_KEY:string = "40p4r0p5t0plws0pqs0prf0tg0pedf0srgv4wrfb0eof4wgs0pq0pq0";
+export default {HOST_NAME,PORT,MONGO_DB_URL,SECRETE_KEY}
--- router/router/userRouter.ts
+++ router/router/userRouter.ts
@@ -3,6 +3,9 @@ import { UserView } from "../models/users/userView";
 import { body,validationResult } from "express-validator";
 import User from "../models/users/User";
 import { IUser } from "../models/users/IUser";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config";
 let userRouter:express.Router = express.Router();
 userRouter.post("/register",[
     body("firstName").not().isEmpty().withMessage("First name Can't left empty"),
@@ -33,16 +36,127 @@ userRouter.post("/register",[
         if(user)
         {
             userData = {} as UserView;
-            userData.errorMessage = "userName or email already exist";
+            userData.errorMessage = "user name or email already exist";
+            return res.status(400).json(userData);
+        }
+        else if(userData.userName == userData.password)
+        {
+            userData = {} as UserView;
+            userData.errorMessage = "user name and password is same";
             return res.status(400).json(userData);
         }
         else
         {
+            let salt:string = await bcrypt.genSalt(10);
+            userData.password = await bcrypt.hash(userData.password,salt);
             user = new User(userData);
             user.save();
             return res.status(200).json(userData);
         }
-        return res.status(200).json(userData);
+    }
+    catch(err)
+    {
+        return res.status(500).json(err);
+    }
+});
+userRouter.post("/login",[
+    body("userName").not().isEmpty().withMessage("username can't left empty"),
+    body("password").not().isEmpty().withMessage("Password can't left empty"),
+],async(req:express.Request,res:express.Response)=>{
+    try
+    {
+        let errors = validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json(errors);
+        }
+        let userData:UserView = {
+            firstName:"",
+            lastName:"",
+            email:"",
+            userName:req.body.userName,
+            password:req.body.password,
+            token:"",
+            errorMessage:""
+        }
+        let user:IUser | null = await User.findOne({userName:userData.userName});
+        if(user)
+        {
+            if(await bcrypt.compare(userData.password,user.password))
+            {
+                let payLoad = {
+                    email:user.email,
+                    userName:user.userName
+                }
+                userData = {} as UserView;
+                userData.token = await jwt.sign(payLoad,config.SECRETE_KEY);
+                return res.status(200).json(userData);
+            }
+            else
+            {
+                userData = {} as UserView;
+                userData.errorMessage = "Invalid password";
+                return res.status(400).json(userData);
+            }
+        }
+        else
+        {
+            userData = {} as UserView;
+            userData.errorMessage = "user name dose not exist";
+            return res.status(400).json(userData);
+        }
+    }
+    catch(err)
+    {
+        return res.status(500).json(err);
+    }
+});
+userRouter.post("/get-user-data",async(req:express.Request,res:express.Response)=>{
+    try
+    {
+        let userData:UserView = {
+            firstName:"",
+            lastName:"",
+            email:"",
+            userName:"",
+            password:"",
+            token:req.body.token,
+            errorMessage:""
+        }
+        if(userData.token=="")
+        { 
+            userData.errorMessage = "Invalid token";
+            return res.status(500).json(userData);
+        }
+        let payLoad: string | jwt.JwtPayload = jwt.verify(userData.token,config.SECRETE_KEY);
+        if(typeof(payLoad) == "string")
+        {
+            userData.errorMessage = "Invalid token";
+            return res.status(500).json(userData);
+        }
+        else
+        {
+            let user:IUser | null = await User.findOne({userName:payLoad.userName,email:payLoad.email}).select("-password");
+            if(user)
+            {
+                userData = {
+                    firstName:user.firstName,
+                    lastName:user.lastName,
+                    email:user.email,
+                    userName:user.userName,
+                    password:"",
+                    errorMessage:"",
+                    token:""
+                }
+                return res.status(200).json(userData);
+            }
+            else
+            {
+                userData = {} as UserView;
+                userData.errorMessage = "Invalid token";
+                return res.status(500).json(userData);
+            }
+        }
     }
     catch(err)
     {
