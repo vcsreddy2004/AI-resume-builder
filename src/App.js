397eec08b35082faa7451b0846f10c051f8fed28
techchandu <vcsreddy2004@gmail.com>
2025-03-20 16:27:41 +0530

    decreased latency by implementing lazy load

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import './App.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.css';
 import './../node_modules/font-awesome/css/font-awesome.css';
-import { BrowserRouter,Routes,Route, Navigate } from 'react-router-dom';
-import ViewResume from './components/view-resume';
-import ResumeForm from './components/resume-form';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar';
-import Profile from './components/profile';
-import LogIn from './components/login';
-import Register from './components/register';
 import LogOut from './components/logout';
 import ProtectedRoute from './components/protectedRoute';
-import ResumeList from './components/resumeList';
+import React, { Suspense } from 'react';
+const LogIn = React.lazy(() => import("./components/login"));
+const Register = React.lazy(() => import("./components/register"));
+const Profile = React.lazy(() => import("./components/profile"));
+const ResumeForm = React.lazy(() => import("./components/resume-form"));
+const ViewResume = React.lazy(() => import("./components/view-resume"));
+const ResumeList = React.lazy(() => import("./components/resumeList"));
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <BrowserRouter>
-        <Routes>
-          <Route path='/logout' element={<LogOut />}></Route>
-          <Route path='/login' element={<LogIn />}></Route>
-          <Route path='/register' element={<Register />}></Route>
-          <Route element={<ProtectedRoute />}>  
-            <Route path='/' element={<ResumeForm />}></Route>
-            <Route path='/profile' element={<Profile/>}></Route>
-            <Route exact path='/view-resume/:resumeId' element={<ViewResume/>}></Route>
-            <Route path='/resume-list' element={<ResumeList></ResumeList>}></Route>
-          </Route>
-          <Route path='*' element={<Navigate to="/login" />}></Route>
-        </Routes>
+        <Suspense fallback={<div className="d-flex align-items-center justify-content-center vh-100"><div className="spinner-border text-success display-1"></div></div>}>
+          <Routes>
+            <Route path='/logout' element={<LogOut />} />
+            <Route path='/login' element={<LogIn />} />
+            <Route path='/register' element={<Register />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path='/' element={<ResumeForm />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route exact path='/view-resume/:resumeId' element={<ViewResume />} />
+              <Route path='/resume-list' element={<ResumeList />} />
+            </Route>
+            <Route path='*' element={<Navigate to="/login" />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   );
